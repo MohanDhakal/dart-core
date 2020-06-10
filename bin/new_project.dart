47d6c1f83c2cae45ac4b6cce855bf6d2504a73ab
MohanDhakal <mohandkl512@gmail.com>
2020-06-10 16:14:19 +0545

    variable learned

--- bin/new_project.dart
+++ bin/new_project.dart
@@ -1,7 +1,8 @@
 import 'package:new_project/new_project.dart' as new_project;
 
-void main(List<String> arguments) {
-
-  print('Hello world: ${new_project.calculate()*2}!');
-
+void main() {
+  // var foo=const[];
+  // print(foo);
+  // foo=[1,2,3];
+  // print(foo);
 }
