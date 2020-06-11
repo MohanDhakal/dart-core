5f1318fce13ef66f22cfc1baa19d6d86d928060e
MohanDhakal <mohandkl512@gmail.com>
2020-06-11 08:17:44 +0545

    async programming

--- bin/new_project.dart
+++ bin/new_project.dart
@@ -1,16 +1,20 @@
-import 'package:new_project/new_project.dart' as new_project;
+// import 'package:new_project/new_project.dart' as new_project;
 
-void main() {
-  // var foo=const[];
-  // print(foo);
-  // foo=[1,2,3];
-  // print(foo);
+// void main() {
+//   // var foo=const[];
+//   // print(foo);
+//   // foo=[1,2,3];
+//   // print(foo);
 
-  // for (int i = 0; i < 5; i++) {
-  //   if (i < 3) {
-  //     continue;
-  //   }
-  //    print(i);
+//   // for (int i = 0; i < 5; i++) {
+//   //   if (i < 3) {
+//   //     continue;
+//   //   }
+//   //    print(i);
 
-  // }
-}
+//   // }
+
+
+
+
+// }
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+void main() {
+//   void printElement(int element) {
+//     print(element);
+//   }
+
+//   var list = [1, 2, 3];
+
+// // Pass printElement as a parameter.
+//   list.forEach(printElement);
+  // Future<int> getSelectedInteger() async {
+  //   await Future.delayed(Duration(seconds: 2));
+  //   return 3;
+  // }
+
+  // void printSelectedNumber() async {
+  //   var a = await getSelectedInteger();
+  //   print(a);
+  // }
+
+  // printSelectedNumber();
+
+}
